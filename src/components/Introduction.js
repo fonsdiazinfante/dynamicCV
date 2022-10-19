463ccd6875e421250e9d85af5cdda9db86bb8bb8
Alfonso Diaz-Infante <alfonsodiazinfante@hotmail.com>
2022-10-19 09:12:20 -0400

    Changed the introduction description

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,8 +3,8 @@ import linkedin from '../img/linkedin.png';
 
 const Intro = () => (
         <div className='text-box'>
-            <p className="white-text"> Computer Science major with a year of co-op experience seeking to leverage my experience in Software Design, React, Node, Ionic, Express, HTML, SQL, Javascript, C, C#, C++, Python and Java. 
-Highly motivated leader who is passionate about the future of computer technology and is looking forward to learn more about the Software Development Life Cycle and the engineering behind REST API's, Back end development and Testing automation.
+            <p className="white-text"> Welcome to my dynamic resume.
+            <br /> <br /> You can follow me on my socials or scrolling to know more about me.
             </p>
             <div className='container'>
                 <img onClick={() => window.location.replace("https://github.com/fonsdiazinfante")} className='socialmedia-logo' alt="github" src={github}/>
