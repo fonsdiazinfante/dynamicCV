aaa556d6ad7b6ac10ae8f3913924768f98e018b0
Alfonso Diaz-Infante <alfonsodiazinfante@hotmail.com>
2022-10-19 09:16:04 -0400

    Commented out pictures of schools

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,13 +5,13 @@ const Education = () => (
         <div>
             <div class="grid-container">
                 <div className="grid-item">George Brown College</div>
-                <img className='school-logo' alt="GBC logo" src={GBC}></img>
+                {/* <img className='school-logo' alt="GBC logo" src={GBC}></img> */}
                 <span className="course-text">Postgraduate study on machine learning (Jan 2022 - Aug 2022)</span>
             </div>
             <div class="grid-container">
                 <div className="grid-item">Tecnológico de Monterrey </div>
-                <img className='school-logo' alt="Tec logo" src={TEC}></img>
-                <span className="course-text">Bachelors Degree on computer science (Aug 2015 - Jul 2021)</span>
+                {/* <img className='school-logo' alt="Tec logo" src={TEC}></img> */}
+                <span className="course-text">Bachelors Degree in computer science (Aug 2015 - Jul 2021)</span>
             </div>
         </div>
 );
